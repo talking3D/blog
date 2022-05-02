672c55647766779450870d26f3df62ee6655c20d
Krzysztof Skwarski <krzysztof@kwartyl.pl>
2022-05-02 18:52:41 +0200

    comment added

--- src/components/SyntaxHighlighter.tsx
+++ src/components/SyntaxHighlighter.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Highlight, { defaultProps } from 'prism-react-renderer';
 import theme from 'prism-react-renderer/themes/vsDark';
 
-
+// TO:DO: Define types for <SyntaxHighlighter> component 'props'
 const SyntaxHighlighter = (props) => {
   const className = props.children.props.className || '';
   const lang = className.match(/language-(?<lang>.*)/);
