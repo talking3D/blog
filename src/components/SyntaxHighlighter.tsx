06179ee9a659540cdb7421741af62f169c082cd5
Krzysztof Skwarski <krzysztof@kwartyl.pl>
2022-05-02 17:40:34 +0200

    SyntaxHighlighter component (based on prism-react-renderer)

---
+++ src/components/SyntaxHighlighter.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import Highlight, { defaultProps } from 'prism-react-renderer';
+import theme from 'prism-react-renderer/themes/vsDark';
+
+
+const SyntaxHighlighter = (props) => {
+  const className = props.children.props.className || '';
+  const lang = className.match(/language-(?<lang>.*)/);
+  const lineNumbers = props.children.props.lineNumbers;
+
+  return (
+    <Highlight
+      { ...defaultProps }
+      code={ props.children.props.children.trim() }
+      language={ lang && lang.groups && lang.groups.lang ? lang.groups.lang : '' }
+      theme={ theme }
+     >
+       {({className, style, tokens, getLineProps, getTokenProps }) => (
+         <pre className={ className } style={ { ...style, padding: '20px' } }>
+           { tokens.map((line, i) => (
+             <div key={ i } { ...getLineProps({ line, key: i })}>
+               { lineNumbers && <span className='line-number'>{ i + 1 }</span> }
+               { line.map((token, key) => (
+                 <span key={ key } { ...getTokenProps({ token, key })} />
+               )) }
+             </div>
+           ))}
+         </pre>
+       )}
+    </Highlight>
+  );
+};
+
+export default SyntaxHighlighter;
