855ce39496f8e972087b63e91155865db605f14c
Krzysztof Skwarski <talking3d@gmail.com>
2022-06-07 15:45:46 +0200

    tailwind configuration file modified
    Custom color is added and 'Montserrat' google-font added as default blog font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,20 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    colors: {
+     'even-darker': '#231F20', 
+    },
     extend: {
       spacing: {
         '22.5': '5.625rem',
       },
+      fontFamily: {
+        'sans': ['Montserrat', defaultTheme.fontFamily.sans],
+      }
     },
   },
   plugins: [],
