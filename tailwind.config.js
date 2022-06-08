030c6beac9c126d2211bdef55479eb9c0104eba9
Krzysztof Skwarski <talking3d@gmail.com>
2022-06-08 09:15:29 +0200

    Tailwind config amended to properly extend default schemas

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,16 +5,20 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    colors: {
-     'even-darker': '#231F20', 
-    },
     extend: {
+      colors: {
+       'even-darker': '#231F20',
+       'cube-like': '#8CC63F',
+      },
+      borderWidth: {
+        'thiner': '0.5px',
+      },
       spacing: {
         '22.5': '5.625rem',
       },
       fontFamily: {
         'sans': ['Montserrat', defaultTheme.fontFamily.sans],
-      }
+      },
     },
   },
   plugins: [],
