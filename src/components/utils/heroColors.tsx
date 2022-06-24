e67b2582e89f3041c0dcfcb84d50989dd8c9dd4b
Krzysztof Skwarski <talking3d@gmail.com>
2022-06-24 18:08:21 +0200

    hero_color opacity increased from 30%to 40%

--- src/components/utils/heroColors.tsx
+++ src/components/utils/heroColors.tsx
@@ -4,67 +4,67 @@
 const tileHeroColors = {
   'default': {
     solid: 'bg-slate-600',
-    withOpacity:' bg-slate-600/30' 
+    withOpacity:' bg-slate-600/40' 
   },
   H_1C2D2E: {
     solid: 'bg-[#1C2D2E]',
-    withOpacity: 'bg-[#1C2D2E]/30'
+    withOpacity: 'bg-[#1C2D2E]/40'
   },
   H_472D14: {
     solid: 'bg-[#472D14]',
-    withOpacity: 'bg-[#472D14]/30'
+    withOpacity: 'bg-[#472D14]/40'
   },
   H_10272E: {
     solid: 'bg-[#10272E]',
-    withOpacity: 'bg-[#10272E]/30'
+    withOpacity: 'bg-[#10272E]/40'
   },
   H_333B3B: {
     solid: 'bg-[#333B3B]',
-    withOpacity: 'bg-[#333B3B]/30'
+    withOpacity: 'bg-[#333B3B]/40'
   },
   H_0A0708: {
     solid: 'bg-[#0A0708]',
-    withOpacity: 'bg-[#0A0708]/30'
+    withOpacity: 'bg-[#0A0708]/40'
   },
   H_9D373C: {
     solid: 'bg-[#9D373C]',
-    withOpacity: 'bg-[#9D373C]/30'
+    withOpacity: 'bg-[#9D373C]/40'
   },
   H_080E1A: {
     solid: 'bg-[#080E1A]',
-    withOpacity: 'bg-[#080E1A]/30'
+    withOpacity: 'bg-[#080E1A]/40'
   },
   H_753F12: {
     solid: 'bg-[#753F12]',
-    withOpacity: 'bg-[#753F12]/30'
+    withOpacity: 'bg-[#753F12]/40'
   },
   H_073AA6: {
     solid: 'bg-[#073AA6]',
-    withOpacity: 'bg-[#073AA6]/30'
+    withOpacity: 'bg-[#073AA6]/40'
   },
   H_501E1C: {
     solid: 'bg-[#501E1C]',
-    withOpacity: 'bg-[#501E1C]/30'
+    withOpacity: 'bg-[#501E1C]/40'
   },
   H_232628: {
     solid: 'bg-[#232628]',
-    withOpacity: 'bg-[#232628]/30'
+    withOpacity: 'bg-[#232628]/40'
   },
   H_2E2E2D: {
     solid: 'bg-[#2E2E2D]',
-    withOpacity: 'bg-[#2E2E2D]/30'
+    withOpacity: 'bg-[#2E2E2D]/40'
   },
   H_866349: {
     solid: 'bg-[#866349]',
-    withOpacity: 'bg-[#866349]/30'
+    withOpacity: 'bg-[#866349]/40'
   },
   H_2C211B: {
     solid: 'bg-[#2C211B]',
-    withOpacity: 'bg-[#2C211B]/30'
+    withOpacity: 'bg-[#2C211B]/40'
   },
   H_110722: {
     solid: 'bg-[#110722]',
-    withOpacity: 'bg-[#110722]/30'
+    withOpacity: 'bg-[#110722]/40'
   },
 };
 
