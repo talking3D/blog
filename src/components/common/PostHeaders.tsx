7bba2f4d5b930e7348b7e06da4cff17abd9c457d
Krzysztof Skwarski <talking3d@gmail.com>
2022-07-07 20:32:41 +0200

    Further TableOfContents component development
    Now it gets opacity when interests with other blog post page elements such as images or code

---
+++ src/components/common/PostHeaders.tsx
@@ -0,0 +1,31 @@
+import { ChildProps } from 'postcss/lib/document';
+import * as React from 'react';
+
+// These compoments create headers ids which are then used to link page fragments (parts (h3) and sections (h2))
+
+export const Header3 = (props: any ) => {
+  const createElementId = (content: string) => {
+    return content.toLowerCase().replaceAll(" ", "-");
+  }
+  return(
+    <h3 
+      id={createElementId(props.children)}
+      className='col-start-1 col-span-3 md:col-start-2 md:col-span-2 mt-2 mb-1 text-2xl font-normal'>
+        {props.children}
+    </h3>
+  );
+};
+
+export const Header4 = (props: any) => {
+  const createElementId = (content: string) => {
+    return content.toLowerCase().replaceAll(" ", "-");
+  }
+  return(
+    <h4 
+      id={createElementId(props.children)}
+      className='col-start-1 col-span-3 md:col-start-2 md:col-span-2 mt-2 mb-1 text-2xl font-normal'>
+        {props.children}
+    </h4>
+  );
+};
+
--- src/components/common/TableOfContents.tsx
+++ src/components/common/TableOfContents.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { Link } from 'gatsby';
 
 export type TableContentsType = {
   items?: {
@@ -27,11 +28,11 @@ const ItemsList = ({tableOfContents}: TableOfContentsProps) => {
   return (
     <ul className='text-sm'>
       { !!tableOfContents.items && tableOfContents.items.map( (part, id) => (
-        <li key={ id } className='font-semibold'>{ part.title }
+        <li key={ id } className='font-semibold'><Link to={ part.url! }>{ part.title }</Link>
           { !!part.items && 
               <ul key={ id } className='mb-4 mt-2 font-normal leading-5 list-outside list-disc pl-4'>
                 { part.items.map( (section, id) => (
-                  <li key={ id } className='mb-2'>{ section.title }</li> ) )
+                  <li key={ id } className='mb-2'><Link to={ section.url }>{ section.title }</Link></li> ) )
                 }
               </ul>}
         </li>
