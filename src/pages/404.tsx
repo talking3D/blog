9bf0c40355f10751b9043866a874ec84d513e8f0
Krzysztof Skwarski <talking3d@gmail.com>
2022-09-05 11:41:55 +0200

    404 page i18n added

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,6 +2,8 @@
 import * as React from "react";
 import { Link } from "gatsby";
 import { StaticImage } from 'gatsby-plugin-image';
+import { Trans } from 'react-i18next';
+import { t } from 'i18next';
 import useActiveLocalePath from '../components/hooks/useActiveLocalePath';
 
 const NotFoundPage = () => (
@@ -12,14 +14,18 @@ const NotFoundPage = () => (
         alt='Blog author looking through magnifying glass on illuminated 404 text'
       />
       <div className='absolute max-w-1/2 right-4 top-1 mt-0 mr-2 lg:mr-6 sm:mt-2 md:mt-6 lg:mt-12 px-2 font-semibold text-white text-xs xs:text-base sm:text-2xl lg:text-4xl'>
-        <p>It looks like we couldn&apos;t find what you are looking for.</p>
+        <p>{t('404page.couldnt_find')}</p>
         <p>
-          <Link
-            to={useActiveLocalePath('pl') ? '/pl' : '/'}
-            className='underline text-cube-like'
-          >
-            See
-          </Link> the full list of our articles!
+          <Trans i18nKey='404page.see_all_posts'>
+            <Link
+              to={useActiveLocalePath('pl') ? '/pl' : '/'}
+              className='underline text-cube-like'
+            >
+              See
+            </Link>
+            {' '}
+            the full list of posts on our blog!
+          </Trans>
         </p>
       </div>
     </div>
