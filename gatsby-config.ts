958087ecd6194443b60340c061ef1aa061f71dec
Krzysztof Skwarski <talking3d@gmail.com>
2023-01-16 14:55:37 +0100

    gatsby-remark-lottie installed

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -39,6 +39,7 @@ const config: GatsbyConfig = {
               // wrapperStyle: 'position: absolute; border: solid 2px blue;'
             },
           },
+          `gatsby-remark-lottie`,
         ],
       },
     },
